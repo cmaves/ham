4b491f4e67cdd5ba810fa40df9b4092376646b98
Curtis Maves <curtismaves@gmail.com>
2020-03-19 19:26:08 +0000

    Init

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+
+
+pub mod rfm69;
+
+#[derive(Debug)]
+pub enum Error {
+	GpioError(gpio_cdev::errors::Error),
+	InitError(String),
+	IoError(std::io::Error)
+}
+impl From<gpio_cdev::errors::Error> for Error {
+	fn from(v: gpio_cdev::errors::Error) -> Self {
+		Error::GpioError(v)
+	}
+}
+impl From<std::io::Error> for Error {
+	fn from(v: std::io::Error) -> Self {
+		Error::IoError(v)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use gpio_cdev::Chip;
+	use crate::rfm69::Rfm69;
+	use spidev::Spidev;
+
+	#[test]
+	fn make_rfm69() {
+		let mut chip = Chip::new("/dev/gpiochip0").unwrap();
+		let rst = chip.get_line(24).unwrap();
+		let en = chip.get_line(26).unwrap();
+		let g0 = chip.get_line(25).unwrap();
+		let spidev = Spidev::open("/dev/spidev0.0").unwrap();
+		let rfm69 = Rfm69::new(rst, en, g0, spidev).unwrap();
+		for reg in rfm69.read_all().unwrap().iter().enumerate() {
+			println!("Reg {:#04X}: {:#04X}", reg.0 + 1, reg.1);
+		}
+	}
+}
---
+++ src/rfm69.rs
@@ -0,0 +1,183 @@
+
+
+use gpio_cdev::{Chip,Line,LineHandle,LineRequestFlags};
+use spidev::{Spidev,SpidevTransfer,SpidevOptions,SpiModeFlags};
+use crate::Error;
+use std::thread::sleep;
+use std::time::Duration;
+
+pub struct Rfm69 {
+	rst: LineHandle,
+	en: LineHandle,
+	g0: LineHandle,
+	spi: Spidev,
+	_g1: Option<LineHandle>,
+	_g2: Option<LineHandle>,
+	_g3: Option<LineHandle>,
+	_g4: Option<LineHandle>,
+	_g5: Option<LineHandle>
+}
+
+impl Rfm69 {
+	pub fn new(rst: Line, en: Line, g0: Line, mut spi: Spidev) -> Result<Self, Error> {
+		let flags = LineRequestFlags::OUTPUT;
+		let rst = rst.request(flags, 1, "rfm69_reset")?;
+		sleep(Duration::from_millis(1)); // let settle
+		rst.set_value(0)?;
+		sleep(Duration::from_millis(5)); // let settle 
+
+		let en = en.request(flags, 1, "rfm69_enable")?;
+		let flags = LineRequestFlags::INPUT;
+		let g0 = g0.request(flags, 0, "rfm69_g0")?;
+
+
+		let options = SpidevOptions::new()
+			.bits_per_word(8)
+			.max_speed_hz(1)
+			.mode(SpiModeFlags::SPI_MODE_0)
+			.build();
+
+		spi.configure(&options)?;
+
+		let rfm = Rfm69 { spi, rst, en , g0, _g1: None, _g2: None, _g3: None, _g4: None, _g5: None };
+		rfm.validate_dev()?;
+		Ok(rfm)
+	}
+	pub fn get_version(&self) -> Result<u8,std::io::Error> {
+		self.read(Register::Version)
+	}
+	pub fn reset(self) -> Result<Self, Error> {
+		self.rst.set_value(1)?;
+		sleep(Duration::from_millis(1));
+		self.rst.set_value(0)?;
+		sleep(Duration::from_millis(5));
+		self.validate_dev()?;
+		Ok(self)
+	}
+	fn validate_dev(&self) -> Result<(), Error> {
+		let version = self.get_version()?;
+		if version == 0x24 {
+			Ok(())
+		} else {
+			Err(Error::InitError(format!("Version mismatch! expected 0x24, found {:#X}", version)))
+		}
+	}
+	fn read(&self, reg: Register) -> Result<u8, std::io::Error> {
+		let mut buf = [0];
+		self.read_many(reg, &mut buf)?;
+		Ok(buf[0])
+	}
+	fn read_many(&self, reg: Register, buf: &mut [u8]) -> Result<(), std::io::Error> {
+		let reg = [(reg as u8) & 0x7F];
+		let addr_xfer =  SpidevTransfer::write(&reg);
+		let read_xfer = SpidevTransfer::read(buf);
+		let mut xfers = [addr_xfer, read_xfer];
+		self.spi.transfer_multiple(&mut xfers)
+		
+	}
+	fn write_many(&self, reg: Register, buf: &[u8]) -> Result<(), std::io::Error> {
+		let reg = [(reg as u8) | 0x80];
+		let mut addr_xfer = SpidevTransfer::write(&reg);
+		addr_xfer.cs_change = 1;
+		let mut write_xfer = SpidevTransfer::write(buf);
+		write_xfer.cs_change = 0;
+		let mut xfers = [addr_xfer, write_xfer];
+		self.spi.transfer_multiple(&mut xfers)
+	}
+	pub fn aes(&self, key: &[u8; 16]) -> Result<(), std::io::Error> {
+		self.write_many(Register::AesKey1, key)
+	}
+	pub fn read_all(&self) -> Result<[u8; 0x4E],std::io::Error> {
+		let mut ret = [0; 0x4E];	
+		self.read_many(Register::OpMode, &mut ret)?;
+		Ok(ret)
+	}
+}
+
+pub enum Register {
+    Fifo = 0x00,
+    OpMode = 0x01,
+    DataModul = 0x02,
+    BitrateMsb = 0x03,
+    BitrateLsb = 0x04,
+    FdevMsb = 0x05,
+    FdevLsb = 0x06,
+    FrfMsb = 0x07,
+    FrfMid = 0x08,
+    FrfLsb = 0x09,
+    Osc1 = 0x0A,
+    AfcCtrl = 0x0B,
+    LowBat = 0x0C,
+    Listen1 = 0x0D,
+    Listen2 = 0x0E,
+    Listen3 = 0x0F,
+    Version = 0x10,
+    PaLevel = 0x11,
+    PaRamp = 0x12,
+    Ocp = 0x13,
+    AgcRef = 0x14,
+    AgcThresh1 = 0x15,
+    AgcThresh2 = 0x16,
+    AgcThresh3 = 0x17,
+    Lna = 0x18,
+    RxBw = 0x19,
+    AfcBw = 0x1A,
+    OokPeak = 0x1B,
+    OokAvg = 0x1C,
+    OokFix = 0x1D,
+    AfcFei = 0x1E,
+    AfcMsb = 0x1F,
+    AfcLsb = 0x20,
+    FeiMsb = 0x21,
+    FeiLsb = 0x22,
+    RssiConfig = 0x23,
+    RssiValue = 0x24,
+    DioMapping1 = 0x25,
+    DioMapping2 = 0x26,
+    IrqFlags1 = 0x27,
+    IrqFlags2 = 0x28,
+    RssiThresh = 0x29,
+    RxTimeout1 = 0x2A,
+    RxTimeout2 = 0x2B,
+    PreambleMsb = 0x2C,
+    PreambleLsb = 0x2D,
+    SyncConfig = 0x2E,
+    SyncValue1 = 0x2F,
+    SyncValue2 = 0x30,
+    SyncValue3 = 0x31,
+    SyncValue4 = 0x32,
+    SyncValue5 = 0x33,
+    SyncValue6 = 0x34,
+    SyncValue7 = 0x35,
+    SyncValue8 = 0x36,
+    PacketConfig1 = 0x37,
+    PayloadLength = 0x38,
+    NodeAddrs = 0x39,
+    BroadcastAddrs = 0x3A,
+    AutoModes = 0x3B,
+    FifoThresh = 0x3C,
+    PacketConfig2 = 0x3D,
+    AesKey1 = 0x3E,
+    AesKey2 = 0x3F,
+    AesKey3 = 0x40,
+    AesKey4 = 0x41,
+    AesKey5 = 0x42,
+    AesKey6 = 0x43,
+    AesKey7 = 0x44,
+    AesKey8 = 0x45,
+    AesKey9 = 0x46,
+    AesKey10 = 0x47,
+    AesKey11 = 0x48,
+    AesKey12 = 0x49,
+    AesKey13 = 0x4A,
+    AesKey14 = 0x4B,
+    AesKey15 = 0x4C,
+    AesKey16 = 0x4D,
+    Temp1 = 0x4E,
+    Temp2 = 0x4F,
+    TestLna = 0x58,
+    TestPa1 = 0x5A,
+    TestPa2 = 0x5C,
+    TestDagc = 0x6F,
+}
+
